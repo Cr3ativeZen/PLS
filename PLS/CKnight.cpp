64c0a6d805a53dcd7430d520c94748cc73adf6e1
hubfra <51798356+hubfra@users.noreply.github.com>
2020-04-21 14:16:14 +0200

    SpinSlash added

--- PLS/CKnight.cpp
+++ PLS/CKnight.cpp
@@ -54,3 +54,54 @@ void __fastcall CKnight::PowerfulUpwardSlash(int pSkill, int pPacket, int pPos)
 	}
 	CSkill::ObjectRelease(Target.GetOffset(), (int)pTarget + 352);
 }
+
+void __fastcall CKnight::SpinSlash(int pSkill, int pPacket, int pPos)
+{
+	int pSkill = GetSkillPointer(38);
+
+	if (IsValid() && pSkill)
+	{
+		ISkill xSkill((void*)pSkill);
+		int nMana = (xSkill.GetGrade() == 1) ? 209 : (200 + (xSkill.GetGrade() * 20));
+		int nSkillGrade = xSkill.GetGrade();
+
+		if (!nSkillGrade)
+			return;
+
+		if (GetCurMp() < nMana)
+			return;
+
+		if (IsValid() && CPlayer::IsWState((int)GetOffset(), 12))
+		{
+			int Around = GetObjectListAround(3);
+
+			while (Around)
+			{
+				ICharacter Object((void*)*(DWORD*)Around);
+
+				if (Object.IsValid() && IsValid() && (*(int(__thiscall**)(int, int, DWORD))(*(DWORD*)GetOffset() + 176))((int)GetOffset(), (int)Object.GetOffset(), 0))
+				{
+					/*int nDmg = static_cast<int>(((GetAttack() * SSBaseDmgMultiPvE) + (CChar::GetDex((int)GetOffset()) * SSAgiMultiPvE) + (CChar::GetStr((int)GetOffset()) * SSStrMultiPvE) + (xSkill.GetGrade() * SSPerGradeMultiPvE)) * (0.5 + (GetDeathBlow() * 0.1)));
+
+					if (IsBuff(69) && Object.GetType() == 1)
+					{
+						nDmg = nDmg + SSAdditonalDMG;
+					}
+
+
+					if (Object.GetType() == 0)
+						nDmg = static_cast<int>((GetAttack() * SSBaseDmgMultiPvP) + (CChar::GetDex((int)GetOffset()) * SSAgiMultiPvP) + (CChar::GetStr((int)GetOffset()) * SSStrMultiPvP) + (xSkill.GetGrade() * SSPerGradeMultiPvP) * (0.5 + (GetDeathBlow() * 0.1)));*/
+
+					int nDmg = 5000;
+					OktayDamageArea(Object, nDmg, 38);
+				}
+
+				Around = CBaseList::Pop((void*)Around);
+			}
+
+			_ShowBattleAnimation(this, 38);
+			RemoveDeathBlow(GetDeathBlow());
+			DecreaseMana(nMana);
+		}
+	}
+}
--- PLS/CKnight.h
+++ PLS/CKnight.h
@@ -21,9 +21,22 @@ public:
 
 	}
 
+	//void __fastcall BrutalAttack(int pSkill, int pPacket, int pPos);
+	//void __fastcall Calls(int pSkill, int pPacket, int pPos);
+	//void __fastcall HalfSwing(int pSkill, int pPacket, int pPos);
+	//void __fastcall LightningSlash(int pSkill, int pPacket, int pPos);
 	void __fastcall PowerfulUpwardSlash(int pSkill, int pPacket, int pPos);
-
-
+	//void __fastcall PowerfulWideningWound(int pSkill, int pPacket, int pPos);
+	//void __fastcall ProvocationOfBlow(int pSkill, int pPacket, int pPos);
+	//void __fastcall Sacrifice(int pSkill, int pPacket, int pPos);
+	//void __fastcall ShieldAttack(int pSkill, int pPacket, int pPos);
+	//void __fastcall ShoutOfDefense(int pSkill, int pPacket, int pPos);
+	//void __fastcall ShoutOfFightingSpirit(int pSkill, int pPacket, int pPos);
+	void __fastcall SpinSlash(int pSkill, int pPacket, int pPos);
+	//void __fastcall SwordDance(int pSkill, int pPacket, int pPos);
+	//void __fastcall TheWaveOfEarth(int pSkill, int pPacket, int pPos);
+	//void __fastcall TheBoomOfEarth(int pSkill, int pPacket, int pPos);
+	//void __fastcall TranscendentalBlow(int pSkill, int pPacket, int pPos);
 
 };
 
